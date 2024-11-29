b2e539a32cef88afe5543ab4c25cc7d2bc235ef8
Thomas Menanteau <91687641+VisualDev-FR@users.noreply.github.com>
2024-11-29 14:51:12 +0100

    frontend: setup register tests

--- front/src/app/features/auth/components/register/register.component.spec.ts
+++ front/src/app/features/auth/components/register/register.component.spec.ts
@@ -1,6 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormControl, ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
@@ -11,8 +11,15 @@ import { expect } from '@jest/globals';
 import { RegisterComponent } from './register.component';
 
 describe('RegisterComponent', () => {
+
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
+
+  let firstNameField: FormControl;
+  let lastNameField: FormControl;
+  let emailField: FormControl;
+  let passwordField: FormControl;
+  let submitButton: HTMLInputElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -20,7 +27,7 @@ describe('RegisterComponent', () => {
       imports: [
         BrowserAnimationsModule,
         HttpClientModule,
-        ReactiveFormsModule,  
+        ReactiveFormsModule,
         MatCardModule,
         MatFormFieldModule,
         MatIconModule,
@@ -32,9 +39,21 @@ describe('RegisterComponent', () => {
     fixture = TestBed.createComponent(RegisterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    firstNameField = component.form.controls["firstName"];
+    lastNameField = component.form.controls["lastName"];
+    emailField = component.form.controls["email"];
+    passwordField = component.form.controls["password"];
+    submitButton = fixture.nativeElement.querySelector(".register-form button[type='submit']");
   });
 
-  it('should create', () => {
+  it('should create component with required fields and submit button', () => {
     expect(component).toBeTruthy();
+    expect(firstNameField).toBeTruthy();
+    expect(lastNameField).toBeTruthy();
+    expect(emailField).toBeTruthy();
+    expect(passwordField).toBeTruthy();
+    expect(submitButton).toBeTruthy();
   });
+
 });
