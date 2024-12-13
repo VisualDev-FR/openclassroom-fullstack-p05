a0eaf8b6210e2818cef8f6ba44229a081e196c02
Thomas Menanteau <91687641+VisualDev-FR@users.noreply.github.com>
2024-12-13 10:36:00 +0100

    frontend: setup cypress tests for login.component

--- front/cypress/e2e/login.cy.ts
+++ front/cypress/e2e/login.cy.ts
@@ -1,27 +1,43 @@
+/// <reference types="cypress" />
+
 describe('Login spec', () => {
+
+  const mockSession = {
+    id: 1,
+    username: 'userName',
+    firstName: 'firstName',
+    lastName: 'lastName',
+    admin: true
+  };
+
   it('Login successfull', () => {
-    cy.visit('/login')
 
-    cy.intercept('POST', '/api/auth/login', {
-      body: {
-        id: 1,
-        username: 'userName',
-        firstName: 'firstName',
-        lastName: 'lastName',
-        admin: true
-      },
-    })
-
+    cy.visit('/login');
+    cy.intercept('POST', '/api/auth/login', { body: mockSession, });
     cy.intercept(
       {
         method: 'GET',
         url: '/api/session',
       },
-      []).as('session')
+      []).as('session');
 
-    cy.get('input[formControlName=email]').type("yoga@studio.com")
-    cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
+    cy.get('input[formControlName=email]').type("yoga@studio.com");
+    cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`);
 
-    cy.url().should('include', '/sessions')
-  })
+    cy.url().should('include', '/sessions');
+  });
+
+  it('Login fail', () => {
+
+    cy.visit('/login')
+    cy.intercept('POST', '/api/auth/login', { statusCode: 403, body: null, });
+
+    cy.get('input[formControlName=email]').type("yoga@studio.com");
+    cy.get('input[formControlName=password]').type("test!1234");
+    cy.get("button[type=submit]").click();
+
+    cy.url().should('include', '/login');
+    cy.get("p.error").should("contain.text", "An error occurred");
+  });
+
 });
