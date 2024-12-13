3465732857eda9dced22e936795af230588f2ddc
Thomas Menanteau <91687641+VisualDev-FR@users.noreply.github.com>
2024-12-13 09:18:03 +0100

    frontend: setup detail.component tests

--- front/src/app/features/sessions/components/detail/detail.component.spec.ts
+++ front/src/app/features/sessions/components/detail/detail.component.spec.ts
@@ -1,25 +1,60 @@
 import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { RouterTestingModule, } from '@angular/router/testing';
-import { expect } from '@jest/globals'; 
+import { RouterTestingModule } from '@angular/router/testing';
+import { expect } from '@jest/globals';
+import { of } from 'rxjs';
+import { Teacher } from 'src/app/interfaces/teacher.interface';
+import { TeacherService } from 'src/app/services/teacher.service';
 import { SessionService } from '../../../../services/session.service';
-
+import { Session } from '../../interfaces/session.interface';
+import { SessionApiService } from '../../services/session-api.service';
 import { DetailComponent } from './detail.component';
 
-
 describe('DetailComponent', () => {
+
   let component: DetailComponent;
-  let fixture: ComponentFixture<DetailComponent>; 
-  let service: SessionService;
+  let fixture: ComponentFixture<DetailComponent>;
+  let sessionService: SessionService;
+  let sessionApiService: SessionApiService;
+  let teacherService: TeacherService;
+
+  const mockSession: Session = {
+    id: 1,
+    name: 'name',
+    description: 'description',
+    date: new Date(),
+    teacher_id: 1,
+    users: [1],
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  };
 
   const mockSessionService = {
-    sessionInformation: {
-      admin: true,
-      id: 1
-    }
-  }
+    sessionInformation: { admin: true, id: 1 },
+  };
+
+  const mockTeacher: Teacher = {
+    id: 1,
+    lastName: 'lastName',
+    firstName: 'firstName',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  };
+
+  const mockTeacherService = {
+    detail: jest.fn().mockReturnValue(of(mockTeacher)),
+  };
+
+  const mockSessionApiService = {
+    detail: jest.fn().mockReturnValue(of(mockSession)),
+    delete: jest.fn(),
+    participate: jest.fn(),
+    unParticipate: jest.fn().mockReturnValue(of(null)),
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -27,20 +62,69 @@ describe('DetailComponent', () => {
         RouterTestingModule,
         HttpClientModule,
         MatSnackBarModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        MatIconModule,
+        MatCardModule,
       ],
-      declarations: [DetailComponent], 
-      providers: [{ provide: SessionService, useValue: mockSessionService }],
-    })
-      .compileComponents();
-      service = TestBed.inject(SessionService);
+      declarations: [DetailComponent],
+      providers: [
+        { provide: SessionService, useValue: mockSessionService },
+        { provide: SessionApiService, useValue: mockSessionApiService },
+        { provide: TeacherService, useValue: mockTeacherService },
+      ],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(DetailComponent);
     component = fixture.componentInstance;
+
+    sessionService = TestBed.inject(SessionService);
+    sessionApiService = TestBed.inject(SessionApiService);
+    teacherService = TestBed.inject(TeacherService);
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-});
 
+  it('should call sessionApiService.detail(), then binding to datas to the component', () => {
+
+    component.ngOnInit();
+
+    expect(sessionApiService.detail).toHaveBeenCalledWith(component.sessionId);
+    expect(component.session).toEqual(mockSession);
+    expect(component.teacher).toEqual(mockTeacher);
+    expect(component.isParticipate).toBe(true);
+  });
+
+  it('should inject correct session details into the dom', () => {
+
+    component.ngOnInit();
+    fixture.detectChanges();
+
+    const titleField = fixture.nativeElement.querySelector('h1').textContent.toLowerCase();
+    const descriptionField = fixture.nativeElement.querySelector('.description').textContent
+
+    expect(titleField).toContain(mockSession.name.toLowerCase());
+    expect(descriptionField).toContain(mockSession.description);
+  });
+
+  it('should call sessionApiService.unParticipate', () => {
+
+    component.sessionId = mockSession.id!.toString();
+    component.unParticipate();
+
+    expect(sessionApiService.unParticipate).toHaveBeenCalledWith(
+      mockSession.id!.toString(),
+      mockSessionService.sessionInformation.id.toString()
+    );
+  });
+
+  it('should call window.history.back()', () => {
+    const spy = jest.spyOn(window.history, 'back');
+    component.back();
+
+    expect(spy).toHaveBeenCalled();
+  });
+});
