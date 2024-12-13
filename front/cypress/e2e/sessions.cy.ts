192f05bb865dc3dc692868830a40544d6e71b7f9
Thomas Menanteau <91687641+VisualDev-FR@users.noreply.github.com>
2024-12-13 13:34:29 +0100

    frontend: setup sessions tests

--- front/cypress/e2e/sessions.cy.ts
+++ front/cypress/e2e/sessions.cy.ts
@@ -33,7 +33,9 @@ describe('Login spec', () => {
   ]
 
   beforeEach(() => {
-    cy.intercept('GET', '/api/session', { body: [mockSession] });
+    cy.intercept('GET', '/api/teacher', { body: mockTeacher });
+    cy.intercept('GET', '/api/teacher/1', { body: mockTeacher });
+    cy.intercept('GET', '/api/session', { body: [mockSession,] });
     cy.intercept('GET', '/api/session/1', { body: mockSession });
     cy.intercept('POST', '/api/auth/login', { body: mockSessionInfo, });
 
@@ -44,9 +46,6 @@ describe('Login spec', () => {
   });
 
   it('should create a new yoga session', () => {
-
-    cy.intercept('GET', '/api/teacher', { body: mockTeacher });
-    cy.intercept('POST', '/api/session', { body: mockSession });
 
     cy.url().should('include', '/sessions');
     cy.get("span").contains("Create").click();
@@ -68,6 +67,58 @@ describe('Login spec', () => {
     cy.get("span").contains("Edit").click()
     cy.url().should('include', '/sessions/update/1');
 
+    cy.get("input[formControlName=name]").clear().type("sessionName")
+    cy.get("input[formControlName=date]").clear().type("2024-12-13")
+    cy.get('mat-select').click();
+    cy.get('mat-option').first().click();
+    cy.get('textarea[formControlName=description]').clear().type("description");
+    cy.get("span").contains("Save").click()
+
+    cy.url().should('include', '/sessions');
+  });
+
+  it("should participate to a session", () => {
+
+    cy.intercept('POST', '/api/auth/login', {
+      body: {
+        id: 1,
+        username: 'userName',
+        firstName: 'firstName',
+        lastName: 'lastName',
+        email: "test@example.com",
+        admin: false,
+      },
+    });
+
+    // Login with non-admin user
+    cy.visit('/login');
+    cy.get('input[formControlName=email]').type("yoga@studio.com");
+    cy.get('input[formControlName=password]').type(`${"test!1234"}`);
+    cy.get('button[type="submit"]').click();
+    cy.url().should('include', '/sessions');
+
+    // navigate to the session details
+    cy.get("span").contains("Detail").click()
+    cy.url().should('include', '/sessions/detail/1');
+
+    // click the participate button
+    cy.intercept("POST", "/api/session/1/participate/1", { body: {} });
+    cy.intercept("GET", "/api/session/1", {
+      body: {
+        id: 1,
+        name: "Session 1",
+        date: "2024-05-01T00:00:00.000+00:00",
+        teacher_id: 1,
+        description: "My description",
+        users: [1],
+        createdAt: "2024-05-04T14:11:11",
+        updatedAt: "2024-05-04T14:11:11",
+      }
+    });
+
+    cy.get("span").contains("Participate").click();
+    cy.url().should('include', '/sessions/detail/1');
+    cy.get("span").contains("Do not participate").should("exist");
   });
 
 });
