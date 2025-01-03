2e9bedbb55a709e6100dd14dadff7d2cc3517568
Thomas Menanteau <91687641+VisualDev-FR@users.noreply.github.com>
2025-01-03 10:50:31 +0100

    back: implement SessionController tests

--- back/src/test/java/com/openclassrooms/starterjwt/controller/SessionControllerTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/controller/SessionControllerTest.java
@@ -12,9 +12,12 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.openclassrooms.starterjwt.dto.SessionDto;
 import com.openclassrooms.starterjwt.payload.response.JwtResponse;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.util.Date;
 
 @TestPropertySource(locations = "classpath:application-test.properties")
 @SpringBootTest
@@ -54,5 +57,78 @@ public class SessionControllerTest {
                 .andExpect(jsonPath("$.id").value(1))
                 .andExpect(jsonPath("$.name").value("Session 1"))
                 .andExpect(jsonPath("$.description").value("My description"));
+    }
+
+    @Test
+    void GetUnexistingSessionById() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                .get("/api/session/999")
+                .header("Authorization", "Bearer " + jwtToken))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void GetAllSessions() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                .get("/api/session")
+                .header("Authorization", "Bearer " + jwtToken))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id").value(1))
+                .andExpect(jsonPath("$[1].id").value(2));
+    }
+
+    @Test
+    void CreateSession() throws Exception {
+
+        SessionDto sessionDto = new SessionDto();
+        sessionDto.setName("Session3");
+        sessionDto.setDescription("description");
+        sessionDto.setDate(new Date());
+        sessionDto.setTeacher_id(1L);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/api/session")
+                .header("Authorization", "Bearer " + jwtToken)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(sessionDto)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("Session3"))
+                .andExpect(jsonPath("$.description").value("description"));
+    }
+
+    @Test
+    void ParticipateSession() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/api/session/2/participate/2")
+                .header("Authorization", "Bearer " + jwtToken))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void UpdateSession() throws Exception {
+
+        SessionDto sessionDto = new SessionDto();
+        sessionDto.setId(1L);
+        sessionDto.setName("Updated Session");
+        sessionDto.setDescription("Updated description");
+        sessionDto.setDate(new Date());
+        sessionDto.setTeacher_id(1L);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                .put("/api/session/1")
+                .header("Authorization", "Bearer " + jwtToken)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(sessionDto)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("Updated Session"))
+                .andExpect(jsonPath("$.description").value("Updated description"));
+    }
+
+    @Test
+    void UnparticipateSession() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                .delete("/api/session/1/participate/1")
+                .header("Authorization", "Bearer " + jwtToken))
+                .andExpect(status().isOk());
     }
 }
--- back/src/test/resources/cleanup.sql
+++ back/src/test/resources/cleanup.sql
@@ -12,11 +12,12 @@ VALUES ('Margot', 'DELAHAYE'),
        ('Hélène', 'THIERCELIN');
 
 INSERT INTO USERS (first_name, last_name, admin, email, password)
-VALUES ('Yoga', 'Studio', true, 'admin@example.com', '$2a$10$.Hsa/ZjUVaHqi0tp9xieMeewrnZxrZ5pQRzddUXE/WjDu2ZThe6Iq'),
-       ('toto', 'toto', false, 'user@example.com', '$2a$10$.Hsa/ZjUVaHqi0tp9xieMeewrnZxrZ5pQRzddUXE/WjDu2ZThe6Iq');
+VALUES ('admin', 'admin', true, 'admin@example.com', '$2a$10$.Hsa/ZjUVaHqi0tp9xieMeewrnZxrZ5pQRzddUXE/WjDu2ZThe6Iq'),
+       ('user', 'user', false, 'user@example.com', '$2a$10$.Hsa/ZjUVaHqi0tp9xieMeewrnZxrZ5pQRzddUXE/WjDu2ZThe6Iq');
 
 INSERT INTO SESSIONS (name, description, date, teacher_id)
-VALUES ('Session 1', 'My description', '2024-12-01 10:00:00', 1);
+VALUES ('Session 1', 'My description', '2024-12-01 10:00:00', 1),
+       ('Session 2', 'My description', '2024-12-02 18:00:00', 2);
 
 
 INSERT INTO PARTICIPATE (user_id, session_id) VALUES (1, 1);
