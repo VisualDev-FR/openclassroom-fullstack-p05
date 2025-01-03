5bad7d25cf9fbe8c7762dd4e2c68360b4f995108
Thomas Menanteau <91687641+VisualDev-FR@users.noreply.github.com>
2025-01-03 11:24:26 +0100

    back: setup SessionService tests

--- back/src/test/java/com/openclassrooms/starterjwt/services/SessionServiceTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/services/SessionServiceTest.java
@@ -1,56 +1,64 @@
 package com.openclassrooms.starterjwt.services;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
 
-import com.openclassrooms.starterjwt.exception.NotFoundException;
 import com.openclassrooms.starterjwt.models.Session;
-import com.openclassrooms.starterjwt.models.User;
 import com.openclassrooms.starterjwt.repository.UserRepository;
 
-@ExtendWith(MockitoExtension.class)
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@TestPropertySource(locations = "classpath:application-test.properties")
+@SpringBootTest
+@AutoConfigureMockMvc
+@Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:cleanup.sql")
 public class SessionServiceTest {
 
-    @Mock
+    @Autowired
     private UserRepository userRepository;
 
-    @Mock
+    @Autowired
     private SessionService sessionService;
 
-    private User adminUser;
+    @Test
+    void testGetSessionByID() {
+        Session session = sessionService.getById(1L);
+        assertNotNull(session);
+        assertEquals("Session 1", session.getName());
+    }
 
-    // @BeforeEach
-    // public void setUp() {
-    //     adminUser = userRepository.findByEmail("admin@example.com").orElseThrow(NotFoundException::new);
-    // }
+    @Test
+    public void testCreateSession() {
 
-    // @Test
-    // public void testCreateSession() {
-    //     Session newSession = new Session()
-    //             .setName("New Yoga Session")
-    //             .setDescription("A new description")
-    //             .setDate(new java.util.Date());
+        Session newSession = new Session()
+                .setName("sessionName")
+                .setDescription("description")
+                .setDate(new java.util.Date());
 
-    //     Session createdSession = sessionService.create(newSession);
-    //     assertNotNull(createdSession.getId());
-    //     assertEquals("New Yoga Session", createdSession.getName());
-    // }
+        Session createdSession = sessionService.create(newSession);
+        assertNotNull(createdSession.getId());
+        assertEquals("sessionName", createdSession.getName());
+        assertEquals("description", createdSession.getDescription());
+    }
 
-    // @Test
-    // public void testCreate() {
-    //     assertNotNull(adminUser);
-    //     assertEquals(adminUser.getEmail(), "admin@example.com");
-    // }
+    @Test
+    void testParticipate() {
+        sessionService.participate(2L, 2L);
+        Session session = sessionService.getById(2L);
+        assertTrue(session.getUsers().contains(userRepository.findById(2L).get()));
+    }
 
-    // @Test
-    // public void testDelete() {
-
-    // }
+    @Test
+    void testUnparticipate() {
+        sessionService.noLongerParticipate(1L, 1L);
+        Session session = sessionService.getById(1L);
+        assertFalse(session.getUsers().contains(userRepository.findById(1L).get()));
+    }
 }
