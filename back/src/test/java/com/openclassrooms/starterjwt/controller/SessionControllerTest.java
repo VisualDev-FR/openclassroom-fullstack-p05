1f3a2578f24ff99a186f4336641053d725c2b8ba
Thomas Menanteau <91687641+VisualDev-FR@users.noreply.github.com>
2025-01-03 10:22:14 +0100

    back: setup SessionControllerTest

---
+++ back/src/test/java/com/openclassrooms/starterjwt/controller/SessionControllerTest.java
@@ -0,0 +1,58 @@
+package com.openclassrooms.starterjwt.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.openclassrooms.starterjwt.payload.response.JwtResponse;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@TestPropertySource(locations = "classpath:application-test.properties")
+@SpringBootTest
+@AutoConfigureMockMvc
+@Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:cleanup.sql")
+public class SessionControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private String jwtToken;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        String loginRequest = "{\"email\": \"admin@example.com\", \"password\": \"test!1234\"}";
+        String response = mockMvc.perform(MockMvcRequestBuilders
+                .post("/api/auth/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(loginRequest))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        jwtToken = objectMapper.readValue(response, JwtResponse.class).getToken();
+    }
+
+    @Test
+    public void GetSessionById() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                .get("/api/session/1")
+                .header("Authorization", "Bearer " + jwtToken))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.name").value("Session 1"))
+                .andExpect(jsonPath("$.description").value("My description"));
+    }
+}
