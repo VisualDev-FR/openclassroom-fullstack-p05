84078e1a3dcab376fdacb0376007fc13edf5acd6
Thomas Menanteau <91687641+VisualDev-FR@users.noreply.github.com>
2025-01-03 15:17:42 +0100

    back: increase test coverage for UserController

--- back/src/test/java/com/openclassrooms/starterjwt/controller/UserControllerTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/controller/UserControllerTest.java
@@ -12,9 +12,11 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.openclassrooms.starterjwt.payload.request.LoginRequest;
 import com.openclassrooms.starterjwt.payload.response.JwtResponse;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 
 @TestPropertySource(locations = "classpath:application-test.properties")
 @SpringBootTest
@@ -32,11 +34,15 @@ public class UserControllerTest {
 
     @BeforeEach
     public void setUp() throws Exception {
-        String loginRequest = "{\"email\": \"admin@example.com\", \"password\": \"test!1234\"}";
+
+        LoginRequest loginRequest = new LoginRequest();
+        loginRequest.setEmail("user-delete@example.com");
+        loginRequest.setPassword("test!1234");
+
         String response = mockMvc.perform(MockMvcRequestBuilders
                 .post("/api/auth/login")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(loginRequest))
+                .content(objectMapper.writeValueAsString(loginRequest)))
                 .andExpect(status().isOk())
                 .andReturn()
                 .getResponse()
@@ -57,6 +63,15 @@ public class UserControllerTest {
                 .andExpect(jsonPath("$.lastName").value("adminLast"));
     }
 
+
+    @Test
+    public void GetUserByIDWrong() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                .get("/api/user/coucou")
+                .header("Authorization", "Bearer " + jwtToken))
+                .andExpect(status().isBadRequest());
+    }
+
     @Test
     public void GetAnotherUser() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders
@@ -66,10 +81,34 @@ public class UserControllerTest {
     }
 
     @Test
+    public void DeleteSelf() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                .delete("/api/user/3")
+                .header("Authorization", "Bearer " + jwtToken))
+                .andExpect(status().isOk());
+    }
+
+    @Test
     public void DeleteAnotherUser() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders
                 .delete("/api/user/2")
                 .header("Authorization", "Bearer " + jwtToken))
                 .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    public void DeleteWrongUser() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                .delete("/api/user/wrong")
+                .header("Authorization", "Bearer " + jwtToken))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void DeleteUnexistingUser() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                .delete("/api/user/999")
+                .header("Authorization", "Bearer " + jwtToken))
+                .andExpect(status().isNotFound());
     }
 }
