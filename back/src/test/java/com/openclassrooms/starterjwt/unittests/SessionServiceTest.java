596d51da6f56b8759ad8104875b539ce4de2289a
Thomas Menanteau <91687641+VisualDev-FR@users.noreply.github.com>
2025-01-17 16:20:54 +0100

    back: setup SessionService unit tests

---
+++ back/src/test/java/com/openclassrooms/starterjwt/unittests/SessionServiceTest.java
@@ -0,0 +1,44 @@
+package com.openclassrooms.starterjwt.unittests;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.openclassrooms.starterjwt.models.Session;
+import com.openclassrooms.starterjwt.repository.SessionRepository;
+import com.openclassrooms.starterjwt.repository.UserRepository;
+import com.openclassrooms.starterjwt.services.SessionService;
+
+@ExtendWith(MockitoExtension.class)
+public class SessionServiceTest {
+
+    @Mock
+    private SessionRepository sessionRepository;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private SessionService sessionService;
+
+    @Test
+    void testCreate() {
+
+        Session session = new Session();
+
+        session.setDescription("description");
+
+        when(sessionRepository.save(any())).thenReturn(session);
+
+        sessionService.create(session);
+
+        verify(sessionRepository, times(1)).save(session);
+    }
+}
